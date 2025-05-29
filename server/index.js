9fe46e2057dd3ef06498c9dfc1b862591a401be9
M0hammad-yasin <mohdysn111@gmail.com>
2025-05-29 17:10:14 +0500

    refactor: move Express app setup to separate app.js file

--- server/index.js
+++ server/index.js
@@ -1,22 +1,11 @@
 // server/index.js
 
-const express = require("express");
 const connectDb = require("./src/config/db");
-const app = express();
 const config = require("./src/config/config");
-const PORT = config.PORT || 3000;
+const PORT = config.port;
+const app = require("./src/app");
 // Connect to the database
 connectDb();
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Basic route
-app.get("/", (req, res) => {
-  res.send("Hello from the Node.js server!");
-});
-
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
