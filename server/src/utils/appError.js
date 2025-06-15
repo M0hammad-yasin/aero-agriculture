6f967552e0720887448c1d20786a7ef006abe2ab
M0hammad-yasin <mohdysn111@gmail.com>
2025-06-15 18:42:06 +0500

    feat(utils): add AppError class for handling operational errors

---
+++ server/src/utils/appError.js
@@ -0,0 +1,16 @@
+// src/utils/appError.js
+
+class AppError extends Error {
+    constructor(message, statusCode) {
+      super(message);
+  
+      this.statusCode = statusCode;
+      this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+      this.isOperational = true; // helpful to distinguish from programming bugs
+  
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  
+  module.exports = AppError;
+  
